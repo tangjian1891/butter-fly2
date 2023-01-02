64ce3cc1ca7f4505bf324668496ede2c75a71db0
tangjian_home_pc <tangjian1891@163.com>
2023-01-02 18:57:57 +0800

    commit

--- packages/collapse/dist/index.es.js
+++ packages/collapse/dist/index.es.js
@@ -1,73 +1,63 @@
-import { defineComponent as i, ref as F, resolveComponent as _, openBlock as E, createElementBlock as f, createVNode as n, withCtx as s, createTextVNode as a, pushScopeId as v, popScopeId as B, createElementVNode as C } from "vue";
-import { Collapse as p, CollapseItem as d } from "vant";
-const D = (u, e) => {
-  if (u.install = (t) => {
-    for (const o of [u, ...Object.values(e ?? {})])
-      t.component(o.name, o);
-  }, e)
-    for (const [t, o] of Object.entries(e))
-      u[t] = o;
-  return u;
-}, A = i({
-  components: {
-    [p.name]: p,
-    [d.name]: d
-  },
-  setup() {
-    return {
-      activeNames: F([])
-    };
+import { defineComponent as p, ref as r, openBlock as u, createElementBlock as i, createVNode as s, unref as c, withCtx as n, createTextVNode as a, pushScopeId as f, popScopeId as m, createElementVNode as v } from "vue";
+import { Collapse as h, CollapseItem as _ } from "vant";
+const C = (e, o) => {
+  if (e.install = (l) => {
+    for (const t of [e, ...Object.values(o ?? {})])
+      l.component(t.name, t);
+  }, o)
+    for (const [l, t] of Object.entries(o))
+      e[l] = t;
+  return e;
+}, I = (e) => (f("data-v-2dc41008"), e = e(), m(), e), V = /* @__PURE__ */ I(() => /* @__PURE__ */ v("div", { class: "ddd_title" }, "看看样式能生效吗", -1)), x = /* @__PURE__ */ a(" 代码是写出来给人看的，附带能在机器上运行。 "), k = /* @__PURE__ */ a(" 技术无非就是那些开发它的人的共同灵魂。 "), y = /* @__PURE__ */ a(" 在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。 "), B = /* @__PURE__ */ p({
+  setup(e) {
+    const o = r([]);
+    return (l, t) => (u(), i("div", null, [
+      V,
+      s(c(h), {
+        modelValue: o.value,
+        "onUpdate:modelValue": t[0] || (t[0] = (d) => o.value = d)
+      }, {
+        default: n(() => [
+          s(c(_), {
+            title: "标题1",
+            name: "1"
+          }, {
+            default: n(() => [
+              x
+            ]),
+            _: 1
+          }),
+          s(c(_), {
+            title: "标题2",
+            name: "2"
+          }, {
+            default: n(() => [
+              k
+            ]),
+            _: 1
+          }),
+          s(c(_), {
+            title: "标题3",
+            name: "3"
+          }, {
+            default: n(() => [
+              y
+            ]),
+            _: 1
+          })
+        ]),
+        _: 1
+      }, 8, ["modelValue"])
+    ]));
   }
 });
-const h = (u, e) => {
-  const t = u.__vccOpts || u;
-  for (const [o, l] of e)
-    t[o] = l;
-  return t;
-}, N = (u) => (v("data-v-045677bc"), u = u(), B(), u), I = /* @__PURE__ */ N(() => /* @__PURE__ */ C("div", { class: "ddd_title" }, "看看样式能生效吗", -1)), V = /* @__PURE__ */ a(" 代码是写出来给人看的，附带能在机器上运行。 "), b = /* @__PURE__ */ a(" 技术无非就是那些开发它的人的共同灵魂。 "), k = /* @__PURE__ */ a(" 在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。 ");
-function y(u, e, t, o, l, w) {
-  const c = _("van-collapse-item"), m = _("van-collapse");
-  return E(), f("div", null, [
-    I,
-    n(m, {
-      modelValue: u.activeNames,
-      "onUpdate:modelValue": e[0] || (e[0] = (r) => u.activeNames = r)
-    }, {
-      default: s(() => [
-        n(c, {
-          title: "标题1",
-          name: "1"
-        }, {
-          default: s(() => [
-            V
-          ]),
-          _: 1
-        }),
-        n(c, {
-          title: "标题2",
-          name: "2"
-        }, {
-          default: s(() => [
-            b
-          ]),
-          _: 1
-        }),
-        n(c, {
-          title: "标题3",
-          name: "3"
-        }, {
-          default: s(() => [
-            k
-          ]),
-          _: 1
-        })
-      ]),
-      _: 1
-    }, 8, ["modelValue"])
-  ]);
-}
-const $ = /* @__PURE__ */ h(A, [["render", y], ["__scopeId", "data-v-045677bc"]]), S = D($);
+const N = (e, o) => {
+  const l = e.__vccOpts || e;
+  for (const [t, d] of o)
+    l[t] = d;
+  return l;
+}, w = /* @__PURE__ */ N(B, [["__scopeId", "data-v-2dc41008"]]), b = C(w);
 export {
-  S as BFCollapse,
-  S as default
+  b as BFCollapse,
+  b as default
 };
--- packages/collapse/src/index.vue
+++ packages/collapse/src/index.vue
@@ -1,34 +1,35 @@
 <template>
   <div>
     <div class="ddd_title">看看样式能生效吗</div>
-    <van-collapse v-model="activeNames">
-      <van-collapse-item title="标题1" name="1">
+    <Collapse v-model="activeNames">
+      <CollapseItem title="标题1" name="1">
         代码是写出来给人看的，附带能在机器上运行。
-      </van-collapse-item>
-      <van-collapse-item title="标题2" name="2">
+      </CollapseItem>
+      <CollapseItem title="标题2" name="2">
         技术无非就是那些开发它的人的共同灵魂。
-      </van-collapse-item>
-      <van-collapse-item title="标题3" name="3">
+      </CollapseItem>
+      <CollapseItem title="标题3" name="3">
         在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。
-      </van-collapse-item>
-    </van-collapse>
+      </CollapseItem>
+    </Collapse>
   </div>
 </template>
-<script lang='ts'>
+<script lang='ts' setup>
 import { defineComponent, ref } from 'vue';
 import { Collapse, CollapseItem } from 'vant';
-export default defineComponent({
-  components: {
-    [Collapse.name]: Collapse,
-    [CollapseItem.name]: CollapseItem,
-  },
-  setup() {
-    const activeNames = ref([]);
-    return {
-      activeNames
-    }
-  }
-})
+const activeNames = ref([]);
+// export default defineComponent({
+//   components: {
+//     [Collapse.name]: Collapse,
+//     [CollapseItem.name]: CollapseItem,
+//   },
+//   setup() {
+    
+//     return {
+//       activeNames
+//     }
+//   }
+// })
 </script>
 <style scoped>
 .ddd_title {
